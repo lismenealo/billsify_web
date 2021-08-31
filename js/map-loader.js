1b3b47226733c74078e517981ce23aad86b25b51
lismenealo <lis.mene.alo@gmail.com>
2021-08-31 21:15:39 +0200

    Adds route calculation between office and client location

--- js/map-loader.js
+++ js/map-loader.js
@@ -1,1 +1,45 @@
 // Map loading
+
+// Initialize and add the map
+function initMap(lat, lng) {
+    // The location of Uluru
+    const valencia = { lat: 39.40778525, lng: -0.3615113 };
+    // The map, centered at Uluru
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 15,
+        center: valencia,
+    });
+    // The marker, positioned at Uluru
+    const marker = new google.maps.Marker({
+        position: valencia,
+        map: map,
+    });
+    navigator.geolocation.getCurrentPosition(
+        function (position) {
+            let currentPosition = { lat: position.coords.latitude, lng: position.coords.longitude }
+            calcRoute(map, valencia, currentPosition);
+        },
+        function errorCallback(error) {
+            console.log(error)
+        }
+    );
+}
+
+function calcRoute(map, start,end) {
+
+    var directionsDisplay = new google.maps.DirectionsRenderer();
+    var directionsService = new google.maps.DirectionsService();
+
+    directionsDisplay.setMap(map);
+
+    var request = {
+        origin:start,
+        destination:end,
+        travelMode: google.maps.DirectionsTravelMode.DRIVING
+    };
+    directionsService.route(request, function(response, status) {
+        if (status === google.maps.DirectionsStatus.OK) {
+            directionsDisplay.setDirections(response);
+        }
+    });
+}
